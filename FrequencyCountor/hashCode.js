cb2c8014f4d5c12246ca6ec4859e0d6feffcaaa4
Doulat Nagar <doulat.nagar@habilelabs.io>
2023-02-13 16:46:50 +0530

    Update of HashCode

--- FrequencyCountor/hashCode.js
+++ FrequencyCountor/hashCode.js
@@ -1,10 +1,8 @@
-const arr = [];
 function hashcode(userId, password) {
     const obj  = {};
     let id = password;
     let id2 = Date.now() + new Date()
     obj[userId] = id2;
-    arr.push(obj);
-    return console.log(arr);
+    return console.log(obj);
 }
 hashcode("Doulat", "doulat123");
