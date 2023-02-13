6d98ddf7c78b9a8b09f7f0bb1abaeb8e6fa63dd1
Doulat Nagar <doulat.nagar@habilelabs.io>
2023-02-13 17:05:06 +0530

    update of hashcode

--- FrequencyCountor/hashCode.js
+++ FrequencyCountor/hashCode.js
@@ -1,3 +1,5 @@
+// console.log(Date.now() + new Date());
+
 function hashcode(userId, password) {
     const obj  = {};
     let id = password;
@@ -5,4 +7,7 @@ function hashcode(userId, password) {
     obj[userId] = id2;
     return console.log(obj);
 }
-hashcode("Doulat", "doulat123");
+hashcode("Daiveek", "rameshsharma");
+
+// In the terminal : node hashCode.js >> hashcode
+// which means the append the output into the file
