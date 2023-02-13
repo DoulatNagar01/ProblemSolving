8f16fcc9bd3948f18efae43ce549fc00ecf6ee9f
Doulat Nagar <doulat.nagar@habilelabs.io>
2023-02-13 10:02:27 +0530

    Update

--- FrequencyCountor/removeduplicate.js
+++ FrequencyCountor/removeduplicate.js
@@ -1,16 +1,18 @@
-function removeEle(arr,num) {
-    let len = arr.length;
-    let count = 0;
-if (len===0) {
-return "Array have must 1 index";
-}
-    let newarr= [];
-    for (let i = 0; i < len; i++) {
-        if (arr[i]!=num) {
-            newarr.push(arr[i]);
-            count++;
-        }
-    }
-    console.log(newarr,count);
-}
-removeEle([1,2,5,5,3],5);
+// using push and newArray.
+
+// function removeEle(arr,num) {
+//     let len = arr.length;
+//     let count = 0;
+// if (len===0) {
+// return "Array have must 1 index";
+// }
+//     let newarr= [];
+//     for (let i = 0; i < len; i++) {
+//         if (arr[i]!=num) {
+//             newarr.push(arr[i]);
+//             count++;
+//         }
+//     }
+//     console.log(newarr,count);
+// }
+// removeEle([1,2,5,5,3],5);
